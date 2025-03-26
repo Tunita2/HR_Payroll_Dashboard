aff0ab18611f03a17fc754c4a27a8cebef6d8417
Nguyen Trung Hieu <hieugrth13@gmail.com>
2025-03-26 16:20:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-// import PayrollView from "./pages/PayrollView";
-// import EmployeeLayout from "./layouts/Admin_Dashboard/employee/EmployeeLayout";
-// import PayrollLayout from "./layouts/Admin_Dashboard/payroll/PayrollLayout";
-// import PayrollView from "./pages/PayrollView";
-// import AttendanceLayout from "./layouts/Admin_Dashboard/attendance/AttendanceLayout";
+import PayrollView from "./pages/PayrollView";
+import EmployeeLayout from "./layouts/Admin_Dashboard/employee/EmployeeLayout";
+import PayrollLayout from "./layouts/Admin_Dashboard/payroll/PayrollLayout";
+import AttendanceLayout from "./layouts/Admin_Dashboard/attendance/AttendanceLayout";
 import ReportLayout from "./layouts/Admin_Dashboard/report/ReportLayout";
+import DashboardLayout from "./layouts/Admin_Dashboard/dashboard/DashboardLayout";
 
 function App() {
   return (
     <div className="App">
       {/* <PayrollView /> */}
-      {/* <PayrollDashboard /> */}
       {/* <EmployeeLayout /> */}
       {/* <PayrollLayout /> */}
       {/* <AttendanceLayout /> */}
-      <ReportLayout />
+      {/* <ReportLayout /> */}
+      {/* <DashboardLayout /> */}
     </div>
   );
 }
