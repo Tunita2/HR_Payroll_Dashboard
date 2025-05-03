3ab779e349a8a321419c0c7cff3fee71874bcecd
Nguyen Trung Hieu <hieugrth13@gmail.com>
2025-05-03 14:23:38 +0700

    merge Hieu Binh

--- src/services/config/mssql.js
+++ src/services/config/mssql.js
@@ -2,7 +2,7 @@ const sql = require("mssql/msnodesqlv8");
 
 const config = {
   connectionString:
-    "Driver={SQL Server};Server=DESKTOP-BE98DFG\\HRPAYROLLDB;Database=HUMAN;Uid=sa;Pwd=0147896325;",
+    `Driver={SQL Server};Server=${process.env.SQL_SERVER_HOST};Database=${process.env.SQL_SERVER_DATABASE};Uid=${process.env.SQL_SERVER_USER};Pwd=${process.env.SQL_SERVER_PASSWORD};`,
 };
 
 const conn = new sql.ConnectionPool(config).connect().then((pool) => {
@@ -13,3 +13,4 @@ module.exports = {
   conn: conn,
   sql: sql,
 };
+
